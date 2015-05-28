da82aad3ac512260542dacde44ce127783d1bc0b
Steve <steve@macintosh.lan>
2015-05-28 20:17:44 +0100

    Now show 3 items in table view.

--- Recipe2/ViewController.swift
+++ Recipe2/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let array = ["item1", "item2", "item3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell") as! UITableViewCell
+        cell.textLabel?.text = array[indexPath.item]
+        
+        return cell
+    }
 }
 
